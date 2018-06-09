5a97c1b7d577a371e1b6c4a4aaa298430a69aafa
Alysson <alyssonggs@hotmail.com>
2018-06-09 12:36:21 -0300

    Iniciando criação dos buckets

--- bucket.c
+++ bucket.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-//quick, select, bubble, insert
+void createBuckets(int ***buckets, int num);
 
 int main(int argc, char *argv[]){
 	if(argc < 3){
@@ -14,5 +15,16 @@ int main(int argc, char *argv[]){
 	int rangeNum;
 	sscanf(argv[2], "%d", &rangeNum);
 
+	int **buckets;
+	createBuckets(&buckets, numBuckets);
 	return 0;
+}
+
+void createBuckets(int ***buckets, int num){
+	int i, **temp_buckets;
+	temp_buckets = malloc(num * sizeof(int*));
+	for(i=0; i < num; i++){
+		temp_buckets[i] = malloc(sizeof(int) * 10);	
+	}	
+	*buckets = temp_buckets;
 }
