1d88dcd227ebb44aae51c061b5a683aaa571a02f
Alysson <alyssonggs@hotmail.com>
2018-06-09 13:20:50 -0300

    Criação e separação dos buckets corrigidas. codigo comentado

--- bucket.c
+++ bucket.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define BUCKET_SIZE 5
 
 void createBuckets(int ***buckets, int num);
-void populateBuckets(int ***buckets, int num, int range);
+void generateValues(int **values, int size, int range);
+void populateBuckets(int ***buckets, int numBuckets, int *values, int numValues);
 void printBuckets(int **buckets, int num);
 void cleanBuckets(int **buckets, int num);
 
 int main(int argc, char *argv[]){
-	if(argc < 3){
+	if(argc < 4){
 		printf("Numero de argumentos insuficiente\n");
 		return 1;
 	}
@@ -18,14 +20,25 @@ int main(int argc, char *argv[]){
 	int **buckets;
 	createBuckets(&buckets, numBuckets);
 
+	//Range para geração de numeros aleatorios
 	int rangeNum;
 	sscanf(argv[2], "%d", &rangeNum);
 
-	populateBuckets(&buckets, numBuckets, rangeNum);
+	//Quantidade de valores
+	int valuesSize, *values;
+	sscanf(argv[3], "%d", &valuesSize);	
+
+	//Gera os valores randomicos para serem divididos nos buckets	
+	generateValues(&values, valuesSize, rangeNum);
+
+	//Popula os buckets pelo array gerado no método anterior, dividindo os valores entre os bucets
+	populateBuckets(&buckets, numBuckets, values, valuesSize);
 	printBuckets(buckets, numBuckets);
-	
+
+	//Limpa tuto
 	cleanBuckets(buckets, numBuckets);
 	free(buckets);
+	free(values);
 	return 0;
 }
 
@@ -33,17 +46,31 @@ void createBuckets(int ***buckets, int num){
 	int i, **temp_buckets;
 	temp_buckets = malloc(num * sizeof(int*));
 	for(i=0; i < num; i++){
-		temp_buckets[i] = malloc(sizeof(int) * 10);	
+		temp_buckets[i] = malloc(sizeof(int) * BUCKET_SIZE);	
+		temp_buckets[i][0] = 0;
 	}	
 	*buckets = temp_buckets;
 }
 
-void populateBuckets(int ***buckets, int num, int range){
-	int i, j, **temp = *buckets;
-	for(i=0; i < num; i++){
-		for(j=0;j<10;j++){
-			temp[i][j] = rand()%range;
-		}
+void generateValues(int **values, int size, int range){
+	int i, *temp =  malloc(size *sizeof(int));
+	for(i=0; i < size; i++){
+		//Gera valores entre 0 e o range dos numeros
+		//Ex.: para range = 50 -> temp[i] vai receber valores entre 0 e 50(50 não incluso)
+		temp[i] = rand()%range;
+	}
+	*values=temp;
+}
+
+void populateBuckets(int ***buckets, int numBuckets, int *values, int numValues){
+	int i, j, **temp = *buckets, id;
+	for(i=0;i < numValues;i++){
+		//TODO: definir um jeito melhor de decidir qual bucket recebe qual numero
+		id = values[i] / numBuckets;
+		//Atualiza a contagem dos elementos do bucket
+		temp[id][0]++;
+		//Insere no bucket	
+		temp[id][temp[id][0]] = values[i];
 	}
 	*buckets= temp;
 }
@@ -51,8 +78,8 @@ void populateBuckets(int ***buckets, int num, int range){
 void printBuckets(int **buckets, int num){
 	int i, j;
 	for(i=0; i < num; i++){
-		printf("Bucket %d: ", i);
-		for(j=0; j < 10; j++){
+		printf("Bucket %d, com size %d: ", i, buckets[i][0]);
+		for(j=1; j < BUCKET_SIZE; j++){
 			printf("%d ", buckets[i][j]);
 		}
 		printf("\n");
