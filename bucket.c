aa0f2b0d87561014dcffa1a13dfb34db0f65aeeb
Alysson <alyssonggs@hotmail.com>
2018-06-09 12:49:50 -0300

    Print, povoamento e limpeza de buckets feitos

--- bucket.c
+++ bucket.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 
 void createBuckets(int ***buckets, int num);
+void populateBuckets(int ***buckets, int num, int range);
+void printBuckets(int **buckets, int num);
+void cleanBuckets(int **buckets, int num);
 
 int main(int argc, char *argv[]){
 	if(argc < 3){
@@ -12,11 +15,17 @@ int main(int argc, char *argv[]){
 	int numBuckets;
 	sscanf(argv[1], "%d", &numBuckets);
 
+	int **buckets;
+	createBuckets(&buckets, numBuckets);
+
 	int rangeNum;
 	sscanf(argv[2], "%d", &rangeNum);
 
-	int **buckets;
-	createBuckets(&buckets, numBuckets);
+	populateBuckets(&buckets, numBuckets, rangeNum);
+	printBuckets(buckets, numBuckets);
+	
+	cleanBuckets(buckets, numBuckets);
+	free(buckets);
 	return 0;
 }
 
@@ -27,4 +36,33 @@ void createBuckets(int ***buckets, int num){
 		temp_buckets[i] = malloc(sizeof(int) * 10);	
 	}	
 	*buckets = temp_buckets;
+}
+
+void populateBuckets(int ***buckets, int num, int range){
+	int i, j, **temp = *buckets;
+	for(i=0; i < num; i++){
+		for(j=0;j<10;j++){
+			temp[i][j] = 0;
+		}
+	}
+	*buckets= temp;
+}
+
+void printBuckets(int **buckets, int num){
+	int i, j;
+	for(i=0; i < num; i++){
+		printf("Bucket %d: ", i);
+		for(j=0; j < 10; j++){
+			printf("%d ", buckets[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
+void cleanBuckets(int **buckets, int num){
+	int i;
+	for(i=0; i<num; i++){
+		free(buckets[i]);
+	}
 }
